af5da26cfbf2257872a653cb414687efcefd17bb
Jinkyung <kimradium@naver.com>
2021-04-16 19:52:30 +0900

    플로팅 버튼 1차 제작(누르면 메세지 출력)

--- app/src/main/java/com/example/floating_button/MainActivity.java
+++ app/src/main/java/com/example/floating_button/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.floating_button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.btnFloating);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("안녕하세요");
+            }
+        });
     }
+
+    // 토스트 메서드 작성
+    void showToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    } // 토스트 메서드 끝
 }
